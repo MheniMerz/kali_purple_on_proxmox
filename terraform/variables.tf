620abe50bc71ac9ad925dc997e210d06963786a0
mheni <mheni@mhlap>
2025-03-25 15:02:43 -0400

    add tf variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,9 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "telmate/proxmox"
-      version = "3.0.1-rc6"
-    }
-  }
+variable "VM_USER" {
+  default = "mheni"
+  type = string
 }
 
-variable "proxmox_api_url" {
-    type = string
-}
-
-variable "proxmox_api_token_id" {
-    type = string
-}
-
-variable "proxmox_api_token_secret" {
-    type = string
-}
-
-
-provider "proxmox" {
-    # these variables are defined in a .tfvars file which is in the gitignore file
-    pm_api_url = var.proxmox_api_url
-    pm_api_token_id = var.proxmox_api_token_id
-    pm_api_token_secret = var.proxmox_api_token_secret
-
-    # (Optional) Skip TLS Verification
-    pm_tls_insecure = true
+variable "VM_SSHKEY" {
+  default = "add public key here"
+  type = string
 }
