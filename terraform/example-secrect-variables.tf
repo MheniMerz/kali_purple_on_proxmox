e3cd6b156d7fa876155333e845893f0cdbafba56
mhenimerz <hani.merzouki@gmail.com>
2025-03-28 15:02:31 -0400

    add example api variables

---
+++ terraform/example-secrect-variables.tf
@@ -0,0 +1,4 @@
+# rename file to credentials.auto.tfvars
+proxmox_api_url = "https://192.168.1.1:8006/api2/json"
+proxmox_api_token_id = "user@pam!tf_token"
+proxmox_api_token_secret = "value of the token you get from proxmox"
